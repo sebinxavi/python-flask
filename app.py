33d662a8da7d44d9e64e9db525f3a4ce23789f34
Sebin Xavi <36552457+sebinxavi@users.noreply.github.com>
2021-07-29 00:52:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return '<h1><center>This Sample Flask Application</center></h1>'
+  return '<h1><center>This is Sample Flask Application</center></h1>'
 
 app.run(host='0.0.0.0', port=5000)
